538ef1c4f9054fb08ef85883e1843c75fb480aed
Aymen <145222715+aymen5507@users.noreply.github.com>
2024-04-02 14:06:44 +0200

    commit

--- model.py
+++ model.py
@@ -1,129 +1,70 @@
-
-import tensorflow as tf 
-from tflearn.layers.core import input_data, dropout, fully_connected
-from tflearn.layers.conv import conv_2d, max_pool_2d
-from tflearn.layers.merge_ops import merge_outputs, merge
-from tflearn.layers.normalization import local_response_normalization, batch_normalization
-from tflearn.layers.estimator import regression 
-from tflearn.optimizers import Momentum, Adam
-
+import torch
 from parameters import NETWORK, HYPERPARAMS
 
-def build_model(optimizer=HYPERPARAMS.optimizer, optimizer_param=HYPERPARAMS.optimizer_param, 
-    learning_rate=HYPERPARAMS.learning_rate, keep_prob=HYPERPARAMS.keep_prob,
-    learning_rate_decay=HYPERPARAMS.learning_rate_decay, decay_step=HYPERPARAMS.decay_step):
+import torch.nn as nn
+import torch.optim as optim
 
-    if NETWORK.model == 'A':
-        return build_modelA(optimizer, optimizer_param, learning_rate, keep_prob, learning_rate_decay, decay_step)
-    elif NETWORK.model == 'B':
-        return build_modelB(optimizer, optimizer_param, learning_rate, keep_prob, learning_rate_decay, decay_step)
-    else:
-        print( "ERROR: no model " + str(NETWORK.model))
-        exit()
 
-def build_modelB(optimizer=HYPERPARAMS.optimizer, optimizer_param=HYPERPARAMS.optimizer_param, 
-    learning_rate=HYPERPARAMS.learning_rate, keep_prob=HYPERPARAMS.keep_prob,
-    learning_rate_decay=HYPERPARAMS.learning_rate_decay, decay_step=HYPERPARAMS.decay_step):
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.optimizer = HYPERPARAMS.optimizer
+        self.optimizer_param = HYPERPARAMS.optimizer_param
+        self.learning_rate = HYPERPARAMS.learning_rate
+        self.keep_prob = HYPERPARAMS.keep_prob
+        self.learning_rate_decay = HYPERPARAMS.learning_rate_decay
+        self.decay_step = HYPERPARAMS.decay_step
 
-    images_network = input_data(shape=[None, NETWORK.input_size, NETWORK.input_size, 1], name='input1')
-    images_network = conv_2d(images_network, 64, 3, activation=NETWORK.activation)
-    #images_network = local_response_normalization(images_network)
-    if NETWORK.use_batchnorm_after_conv_layers:
-        images_network = batch_normalization(images_network)
-    images_network = max_pool_2d(images_network, 3, strides = 2)
-    images_network = conv_2d(images_network, 128, 3, activation=NETWORK.activation)
-    if NETWORK.use_batchnorm_after_conv_layers:
-        images_network = batch_normalization(images_network)
-    images_network = max_pool_2d(images_network, 3, strides = 2)
-    images_network = conv_2d(images_network, 256, 3, activation=NETWORK.activation)
-    if NETWORK.use_batchnorm_after_conv_layers:
-        images_network = batch_normalization(images_network)
-    images_network = max_pool_2d(images_network, 3, strides = 2)
-    images_network = dropout(images_network, keep_prob=keep_prob)
-    images_network = fully_connected(images_network, 4096, activation=NETWORK.activation)
-    images_network = dropout(images_network, keep_prob=keep_prob)
-    images_network = fully_connected(images_network, 1024, activation=NETWORK.activation)
-    if NETWORK.use_batchnorm_after_fully_connected_layers:
-        images_network = batch_normalization(images_network)
-
-    if NETWORK.use_landmarks or NETWORK.use_hog_and_landmarks:
-        if NETWORK.use_hog_sliding_window_and_landmarks:
-            landmarks_network = input_data(shape=[None, 2728], name='input2')
-        elif NETWORK.use_hog_and_landmarks:
-            landmarks_network = input_data(shape=[None, 208], name='input2')
+        if NETWORK.model == 'A':
+            self.network = self.build_modelA()
+        elif NETWORK.model == 'B':
+            self.network = self.build_modelB()
         else:
-            landmarks_network = input_data(shape=[None, 68, 2], name='input2')
-        landmarks_network = fully_connected(landmarks_network, 1024, activation=NETWORK.activation)
-        if NETWORK.use_batchnorm_after_fully_connected_layers:
-            landmarks_network = batch_normalization(landmarks_network)
-        landmarks_network = fully_connected(landmarks_network, 128, activation=NETWORK.activation)
-        if NETWORK.use_batchnorm_after_fully_connected_layers:
-            landmarks_network = batch_normalization(landmarks_network)
-        images_network = fully_connected(images_network, 128, activation=NETWORK.activation)
-        network = merge([images_network, landmarks_network], 'concat', axis=1)
-    else:
-        network = images_network
-    network = fully_connected(network, NETWORK.output_size, activation='softmax')
+            print("ERROR: no model " + str(NETWORK.model))
+            exit()
 
-    if optimizer == 'momentum':
-        optimizer = Momentum(learning_rate=learning_rate, momentum=optimizer_param, 
-                    lr_decay=learning_rate_decay, decay_step=decay_step)
-    elif optimizer == 'adam':
-        optimizer = Adam(learning_rate=learning_rate, beta1=optimizer_param, beta2=learning_rate_decay)
-    else:
-        print( "Unknown optimizer: {}".format(optimizer))
-    network = regression(network, optimizer=optimizer, loss=NETWORK.loss, learning_rate=learning_rate, name='output')
+    def forward(self, x):
+        return self.network(x)
 
-    return network
+    def build_modelB(self):
+        model = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Dropout(self.keep_prob),
+            nn.Linear(4096, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, 128),
+            nn.ReLU(),
+            nn.Linear(128, NETWORK.output_size),
+            nn.Softmax(dim=1)
+        )
+        return model
 
-def build_modelA(optimizer=HYPERPARAMS.optimizer, optimizer_param=HYPERPARAMS.optimizer_param, 
-    learning_rate=HYPERPARAMS.learning_rate, keep_prob=HYPERPARAMS.keep_prob,
-    learning_rate_decay=HYPERPARAMS.learning_rate_decay, decay_step=HYPERPARAMS.decay_step):
+    def build_modelA(self):
+        model = nn.Sequential(
+            nn.Conv2d(1, 64, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 64, kernel_size=5, stride=1, padding=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2),
+            nn.Conv2d(64, 128, kernel_size=4, stride=1, padding=2),
+            nn.ReLU(),
+            nn.Dropout(self.keep_prob),
+            nn.Linear(1024, 1024),
+            nn.ReLU(),
+            nn.Linear(1024, NETWORK.output_size),
+            nn.Softmax(dim=1)
+        )
+        return model
 
-    images_network = input_data(shape=[None, NETWORK.input_size, NETWORK.input_size, 1], name='input1')
-    images_network = conv_2d(images_network, 64, 5, activation=NETWORK.activation)
-    #images_network = local_response_normalization(images_network)
-    if NETWORK.use_batchnorm_after_conv_layers:
-        images_network = batch_normalization(images_network)
-    images_network = max_pool_2d(images_network, 3, strides = 2)
-    images_network = conv_2d(images_network, 64, 5, activation=NETWORK.activation)
-    if NETWORK.use_batchnorm_after_conv_layers:
-        images_network = batch_normalization(images_network)
-    images_network = max_pool_2d(images_network, 3, strides = 2)
-    images_network = conv_2d(images_network, 128, 4, activation=NETWORK.activation)
-    if NETWORK.use_batchnorm_after_conv_layers:
-        images_network = batch_normalization(images_network)
-    images_network = dropout(images_network, keep_prob=keep_prob)
-    images_network = fully_connected(images_network, 1024, activation=NETWORK.activation)
-    if NETWORK.use_batchnorm_after_fully_connected_layers:
-        images_network = batch_normalization(images_network)
-
-    if NETWORK.use_landmarks or NETWORK.use_hog_and_landmarks:
-        if NETWORK.use_hog_sliding_window_and_landmarks:
-            landmarks_network = input_data(shape=[None, 2728], name='input2')
-        elif NETWORK.use_hog_and_landmarks:
-            landmarks_network = input_data(shape=[None, 208], name='input2')
-        else:
-            landmarks_network = input_data(shape=[None, 68, 2], name='input2')
-        landmarks_network = fully_connected(landmarks_network, 1024, activation=NETWORK.activation)
-        if NETWORK.use_batchnorm_after_fully_connected_layers:
-            landmarks_network = batch_normalization(landmarks_network)
-        landmarks_network = fully_connected(landmarks_network, 40, activation=NETWORK.activation)
-        if NETWORK.use_batchnorm_after_fully_connected_layers:
-            landmarks_network = batch_normalization(landmarks_network)
-        images_network = fully_connected(images_network, 40, activation=NETWORK.activation)
-        network = merge([images_network, landmarks_network], 'concat', axis=1)
-    else:
-        network = images_network
-    network = fully_connected(network, NETWORK.output_size, activation='softmax')
-
-    if optimizer == 'momentum':
-        optimizer = Momentum(learning_rate=learning_rate, momentum=optimizer_param, 
-                    lr_decay=learning_rate_decay, decay_step=decay_step)
-    elif optimizer == 'adam':
-        optimizer = Adam(learning_rate=learning_rate, beta1=optimizer_param, beta2=learning_rate_decay)
-    else:
-        print( "Unknown optimizer: {}".format(optimizer))
-    network = regression(network, optimizer=optimizer, loss=NETWORK.loss, learning_rate=learning_rate, name='output')
-
-    return network
+model = Model()
+optimizer = optim.SGD(model.parameters(), lr=model.learning_rate, momentum=model.optimizer_param)
+criterion = nn.CrossEntropyLoss()
--- predict.py
+++ predict.py
@@ -1,51 +1,30 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision.datasets import ImageFolder
 
-import tensorflow as tf
-from tflearn import DNN
-import time
-import numpy as np
-import argparse
-import dlib
-import cv2
-import os
-from skimage.feature import hog
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
 
-from parameters import DATASET, TRAINING, NETWORK, VIDEO_PREDICTOR
-from model import build_model
-
-window_size = 24
-window_step = 6
+# Define your PyTorch model here
 
 def load_model():
     model = None
-    with tf.Graph().as_default():
-        print( "loading pretrained model...")
-        network = build_model()
-        model = DNN(network)
-        if os.path.isfile(TRAINING.save_model_path):
-            model.load(TRAINING.save_model_path)
-        else:
-            print( "Error: file '{}' not found".format(TRAINING.save_model_path))
+    print("loading pretrained model...")
+    model = YourModelClass()  # Replace YourModelClass with the actual class of your model
+    model.load_state_dict(torch.load(TRAINING.save_model_path))
     return model
 
-def get_landmarks(image, rects, predictor):
-    # this function have been copied from http://bit.ly/2cj7Fpq
-    if len(rects) > 1:
-        raise TooManyFaces
-    if len(rects) == 0:
-        raise NoFaces
-    return np.matrix([[p.x, p.y] for p in predictor(image, rects[0]).parts()])
-
-def sliding_hog_windows(image):
-    hog_windows = []
-    for y in range(0, NETWORK.input_size, window_step):
-        for x in range(0, NETWORK.input_size, window_step):
-            window = image[y:y+window_size, x:x+window_size]
-            hog_windows.extend(hog(window, orientations=8, pixels_per_cell=(8, 8),
-                                            cells_per_block=(1, 1), visualise=False))
-    return hog_windows
-
 def predict(image, model, shape_predictor=None):
-    # get landmarks
+    # Preprocess the image
+    transform = transforms.Compose([
+        transforms.Grayscale(),
+        transforms.Resize((NETWORK.input_size, NETWORK.input_size)),
+        transforms.ToTensor()
+    ])
+    tensor_image = transform(image).unsqueeze(0)
+
+    # Get landmarks if needed
     if NETWORK.use_landmarks or NETWORK.use_hog_and_landmarks or NETWORK.use_hog_sliding_window_and_landmarks:
         face_rects = [dlib.rectangle(left=0, top=0, right=NETWORK.input_size, bottom=NETWORK.input_size)]
         face_landmarks = np.array([get_landmarks(image, face_rects, shape_predictor)])
@@ -57,39 +36,32 @@ def predict(image, model, shape_predictor=None):
             features = np.concatenate((face_landmarks, hog_features))
         else:
             hog_features, _ = hog(image, orientations=8, pixels_per_cell=(16, 16),
-                                    cells_per_block=(1, 1), visualise=True)
+                                    cells_per_block=(1, 1), visualize=True)
             hog_features = np.asarray(hog_features)
             face_landmarks = face_landmarks.flatten()
             features = np.concatenate((face_landmarks, hog_features))
-        tensor_image = image.reshape([-1, NETWORK.input_size, NETWORK.input_size, 1])
-        predicted_label = model.predict([tensor_image, features.reshape((1, -1))])
+        tensor_features = torch.from_numpy(features).unsqueeze(0)
+        predicted_label = model(tensor_image, tensor_features)
         return get_emotion(predicted_label[0])
     else:
-        tensor_image = image.reshape([-1, NETWORK.input_size, NETWORK.input_size, 1])
-        predicted_label = model.predict(tensor_image)
+        predicted_label = model(tensor_image)
         return get_emotion(predicted_label[0])
-    return None
 
-def get_emotion(label):
-    if VIDEO_PREDICTOR.print_emotions:
-        print( "- Angry: {0:.1f}%\n- Happy: {1:.1f}%\n- Sad: {2:.1f}%\n- Surprise: {3:.1f}%\n- Neutral: {4:.1f}%".format(
-                label[0]*100, label[1]*100, label[2]*100, label[3]*100, label[4]*100))
-    label = label.tolist()
-    return VIDEO_PREDICTOR.emotions[label.index(max(label))], max(label)
+# Load the model
+model = load_model()
 
-# parse arg to see if we need to launch training now or not yet
+# Parse arg to see if we need to launch training now or not yet
 parser = argparse.ArgumentParser()
 parser.add_argument("-i", "--image", help="Image file to predict")
 args = parser.parse_args()
 if args.image:
     if os.path.isfile(args.image):
-        model = load_model()
         image = cv2.imread(args.image, 0)
         shape_predictor = dlib.shape_predictor(DATASET.shape_predictor_path)
         start_time = time.time()
         emotion, confidence = predict(image, model, shape_predictor)
         total_time = time.time() - start_time
-        print( "Prediction: {0} (confidence: {1:.1f}%)".format(emotion, confidence*100))
-        print( "time: {0:.1f} sec".format(total_time))
+        print("Prediction: {0} (confidence: {1:.1f}%)".format(emotion, confidence*100))
+        print("time: {0:.1f} sec".format(total_time))
     else:
-        print( "Error: file '{}' not found".format(args.image))
+        print("Error: file '{}' not found".format(args.image))
